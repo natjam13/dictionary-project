b34e3e4032d2ea6be5076ead1013479f1fe24d69
natjam13 <murrayn2@sky.com>
2025-06-01 08:03:56 +0100

    created search engine

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import logo from "./logo.png";
 import './App.css';
+import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-Header">
-      <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-Header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          Compiled by Natalie
+        </footer>
+      </div>
     </div>
   );
 }
 
-export default App;
+
