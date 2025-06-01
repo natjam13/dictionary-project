9fd62b2bc7c89048690f6b9785b678c3befe53c0
natjam13 <murrayn2@sky.com>
2025-06-01 11:26:47 +0100

    refreshed code

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -8,17 +8,16 @@ export default function Dictionary() {
   let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-      console.log(response.data[0]);
-      setResults(response.data[0]);
+    setResults(response.data[0]);
   }
 
-    function search(event) {
-        event.preventDefault();
+  function search(event) {
+    event.preventDefault();
 
-        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-        axios.get(apiUrl).then(handleResponse);
+    
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
-
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
@@ -28,8 +27,8 @@ export default function Dictionary() {
     <div className="Dictionary">
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
-          </form>
-          <Results />
+      </form>
+      <Results results={results} />
     </div>
   );
 }
